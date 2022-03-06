7791a45987ed49edc6581e6b90084929c955b9a1
Travis Courtney <travis@angry-fly.com>
2022-03-06 14:05:45 -0500

    Completed File Reader lesson

---
+++ src/FileReaderClass/Main.java
@@ -0,0 +1,31 @@
+package FileReaderClass;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+        FileReader reader;
+
+        {
+            try {
+                reader = new FileReader("art.txt");
+                int data = reader.read();
+                while( data != -1) {
+                    System.out.print((char) data);
+                    data = reader.read();
+                }
+                reader.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+
+}
