e9bd7e4e546e7c7fbeb776d287bc3c7b2faad030
Travis Courtney <travis@angry-fly.com>
2022-03-06 16:41:20 -0500

    Completed Audio lesson

---
+++ src/AudioLesson/Main.java
@@ -0,0 +1,48 @@
+package AudioLesson;
+
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+
+        Scanner scanner = new Scanner(System.in);
+
+        File file = new File("Level_Up.wav");
+
+        AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+        Clip clip = AudioSystem.getClip();
+        clip.open(audioStream);
+
+        String response = "";
+
+        response = response.toUpperCase();
+        while (!response.equals("Q")) {
+            System.out.println("P = play, S = Stop, R = Reset, Q = Quit");
+            System.out.println("Enter your choice: ");
+
+            response = scanner.next();
+            response = response.toUpperCase();
+
+            switch (response) {
+                case ("P"): clip.start();
+                break;
+                case ("S"): clip.stop();
+                break;
+                case ("R"): clip.setMicrosecondPosition(0);
+                break;
+                case ("Q"): clip.close();
+                break;
+                default: System.out.println("Not a valid response");
+            }
+
+        }
+
+        clip.start();
+
+    }
+
+}
