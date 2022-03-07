ddd699edc1cabd921606ac3129166e2945c4c5cd
Travis Courtney <travis@angry-fly.com>
2022-03-07 13:29:15 -0500

    Completed Buttons lesson

---
+++ src/ButtonsLesson/Main.java
@@ -0,0 +1,9 @@
+package ButtonsLesson;
+
+public class Main {
+    public static void main(String[] args) {
+        new MyFrame();
+
+
+    }
+}
---
+++ src/ButtonsLesson/MyFrame.java
@@ -0,0 +1,29 @@
+package ButtonsLesson;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MyFrame extends JFrame implements ActionListener {
+    JButton button;
+    MyFrame() {
+        button = new JButton();
+        button.setBounds(200,100, 100, 50);
+        button.addActionListener(this);
+        button.setText("Click me!");
+        button.setFocusable(false);
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(null);
+        this.setSize(500,500);
+        this.setVisible(true);
+        this.add(button);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource()==button) {
+            System.out.println("Get murked");
+        }
+    }
+}
